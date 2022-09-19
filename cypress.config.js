b1fceff07862580a42533af970c57cff7086a76a
aholearnings <111949971+aholearnings@users.noreply.github.com>
2022-09-19 05:50:52 +0530

    Update

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'kha82i',
+  projectId: 'qzjfz2',
   chromeWebSecurity: false,
   e2e: {
     setupNodeEvents(on, config) {
